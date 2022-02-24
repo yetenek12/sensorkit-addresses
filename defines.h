e44e04dbf9dddeb85a3e57458f6ec98e59f9ac8c
Kagan Inan <contact@kaaninan.net>
2022-02-24 09:49:58 +0300

    updates

--- defines.h
+++ defines.h
@@ -70,72 +70,72 @@
 #define ADDR_IO_B8              58
 
 
-// AIR MIC SGP Module
-#define ADDR_AMS_1              40 // Default
-#define ADDR_AMS_2              41
-#define ADDR_AMS_3              42
-#define ADDR_AMS_4              43
-#define ADDR_AMS_5              44
-#define ADDR_AMS_6              45
+// Air
+#define ADDR_AIR_1              40 // Default
+#define ADDR_AIR_2              41
+#define ADDR_AIR_3              42
+#define ADDR_AIR_4              43
+#define ADDR_AIR_5              44
+#define ADDR_AIR_6              45
 
-#define ADDR_AMS_SF_MSB         30 // Sound Frequency MSB
-#define ADDR_AMS_SF_LSB         31 // Sound Frequency LSB
-#define ADDR_AMS_SA_MSB         32 // Sound Amplitude MSB
-#define ADDR_AMS_SA_LSB         33 // Sound Amplitude LSB
-#define ADDR_AMS_TMP_MSB	    34 // BME Temperature MSB
-#define ADDR_AMS_TMP_LSB	    35 // BME Temperature LSB
-#define ADDR_AMS_HUM_MSB	    36 // BME Humidity MSB
-#define ADDR_AMS_HUM_LSB	    37 // BME Humidity LSB
-#define ADDR_AMS_PRE_MSB	    38 // BME Pressure MSB
-#define ADDR_AMS_PRE_LSB	    39 // BME Pressure LSB
-#define ADDR_AMS_ALT_MSB	    40 // BME Altitiude MSB
-#define ADDR_AMS_ALT_LSB	    41 // BME Altitiude LSB
-#define ADDR_AMS_BME_C	        42 // Config: BME Filter Options
-#define ADDR_AMS_BME_SEA_MSB    43 // Config: BME Sea Level MSB
-#define ADDR_AMS_BME_SEA_LSB    44 // Config: BME Sea Level LSB
-#define ADDR_AMS_CO2_MSB        45 // SGP30 CO2 MSB
-#define ADDR_AMS_CO2_LSB        46 // SGP30 CO2 LSB
-#define ADDR_AMS_TVOC_MSB       47 // SGP30 TVOC MSB
-#define ADDR_AMS_TVOC_LSB       48 // SGP30 TVOC LSB
-#define ADDR_AMS_H2_MSB         49 // SGP30 H2 MSB
-#define ADDR_AMS_H2_LSB         50 // SGP30 H2 LSB
-#define ADDR_AMS_ETH_MSB        51 // SGP30 ETHANOL MSB
-#define ADDR_AMS_ETH_LSB        52 // SGP30 ETHANOL LSB
-#define ADDR_AMS_LED            53 // LED
-
-
-// TOF RGB UV Module
-#define ADDR_TRU_1              50 // Default
-#define ADDR_TRU_2              51
-#define ADDR_TRU_3              52
-#define ADDR_TRU_4              53
-#define ADDR_TRU_5              54
-#define ADDR_TRU_6              55
-
-#define ADDR_TRU_DIS_MSB        30 // Distance MSB
-#define ADDR_TRU_DIS_LSB        31 // Distance LSB
-#define ADDR_TRU_DIS_STA        32 // Distance Status
-#define ADDR_TRU_DIS_M          33 // Config: Distance Reading Mode
-#define ADDR_TRU_A_MSB          34 // UV A MSB
-#define ADDR_TRU_A_LSB          35 // UV A LSB
-#define ADDR_TRU_B_MSB          36 // UV B MSB
-#define ADDR_TRU_B_LSB          37 // UV B LSB
-#define ADDR_TRU_I_MSB          38 // UV Index MSB
-#define ADDR_TRU_I_LSB          39 // UV Index LSB
-#define ADDR_TRU_UV_I	        40 // Config: UV Integration Time
-#define ADDR_TRU_UV_S	        41 // Config: UV setHighDynamic
-#define ADDR_TRU_C_R            42 // Color Red
-#define ADDR_TRU_C_G            43 // Color Green
-#define ADDR_TRU_C_B            44 // Color Blue
-#define ADDR_TRU_C_C            45 // Color Clear
-#define ADDR_TRU_C_T            46 // Color Temp
-#define ADDR_TRU_C_L            47 // Color Lux
-#define ADDR_TRU_C_CONF         48 // Config: Color Sensor
-#define ADDR_TRU_IR             49 // IR Sensor
-#define ADDR_TRU_LED            50 // LED
+#define ADDR_AIR_SF_MSB         30 // Sound Frequency MSB
+#define ADDR_AIR_SF_LSB         31 // Sound Frequency LSB
+#define ADDR_AIR_SA_MSB         32 // Sound Amplitude MSB
+#define ADDR_AIR_SA_LSB         33 // Sound Amplitude LSB
+#define ADDR_AIR_TMP_MSB	    34 // BME Temperature MSB
+#define ADDR_AIR_TMP_LSB	    35 // BME Temperature LSB
+#define ADDR_AIR_HUM_MSB	    36 // BME Humidity MSB
+#define ADDR_AIR_HUM_LSB	    37 // BME Humidity LSB
+#define ADDR_AIR_PRE_MSB	    38 // BME Pressure MSB
+#define ADDR_AIR_PRE_LSB	    39 // BME Pressure LSB
+#define ADDR_AIR_ALT_MSB	    40 // BME Altitiude MSB
+#define ADDR_AIR_ALT_LSB	    41 // BME Altitiude LSB
+#define ADDR_AIR_BME_C	        42 // Config: BME Filter Options
+#define ADDR_AIR_BME_SEA_MSB    43 // Config: BME Sea Level MSB
+#define ADDR_AIR_BME_SEA_LSB    44 // Config: BME Sea Level LSB
+#define ADDR_AIR_CO2_MSB        45 // SGP30 CO2 MSB
+#define ADDR_AIR_CO2_LSB        46 // SGP30 CO2 LSB
+#define ADDR_AIR_TVOC_MSB       47 // SGP30 TVOC MSB
+#define ADDR_AIR_TVOC_LSB       48 // SGP30 TVOC LSB
+#define ADDR_AIR_H2_MSB         49 // SGP30 H2 MSB
+#define ADDR_AIR_H2_LSB         50 // SGP30 H2 LSB
+#define ADDR_AIR_ETH_MSB        51 // SGP30 ETHANOL MSB
+#define ADDR_AIR_ETH_LSB        52 // SGP30 ETHANOL LSB
+#define ADDR_AIR_LED            53 // LED
 
 
-// 9DoF Sensor
+// Optics
+#define ADDR_OPT_1              50 // Default
+#define ADDR_OPT_2              51
+#define ADDR_OPT_3              52
+#define ADDR_OPT_4              53
+#define ADDR_OPT_5              54
+#define ADDR_OPT_6              55
+
+#define ADDR_OPT_DIS_MSB        30 // Distance MSB
+#define ADDR_OPT_DIS_LSB        31 // Distance LSB
+#define ADDR_OPT_DIS_STA        32 // Distance Status
+#define ADDR_OPT_DIS_M          33 // Config: Distance Reading Mode
+#define ADDR_OPT_A_MSB          34 // UV A MSB
+#define ADDR_OPT_A_LSB          35 // UV A LSB
+#define ADDR_OPT_B_MSB          36 // UV B MSB
+#define ADDR_OPT_B_LSB          37 // UV B LSB
+#define ADDR_OPT_I_MSB          38 // UV Index MSB
+#define ADDR_OPT_I_LSB          39 // UV Index LSB
+#define ADDR_OPT_UV_I	        40 // Config: UV Integration Time
+#define ADDR_OPT_UV_S	        41 // Config: UV setHighDynamic
+#define ADDR_OPT_C_R            42 // Color Red
+#define ADDR_OPT_C_G            43 // Color Green
+#define ADDR_OPT_C_B            44 // Color Blue
+#define ADDR_OPT_C_C            45 // Color Clear
+#define ADDR_OPT_C_T            46 // Color Temp
+#define ADDR_OPT_C_L            47 // Color Lux
+#define ADDR_OPT_C_CONF         48 // Config: Color Sensor
+#define ADDR_OPT_IR             49 // IR Sensor
+#define ADDR_OPT_LED            50 // LED
+
+
+// IMU
 #define ADDR_DOF9_1             60 // Default
 #define ADDR_DOF9_2             61
 #define ADDR_DOF9_3             62
@@ -143,29 +143,29 @@
 #define ADDR_DOF9_5             64
 #define ADDR_DOF9_6             65
 
-#define ADDR_ACCX_MSB           30
-#define ADDR_ACCX_LSB           31
-#define ADDR_ACCY_MSB           32
-#define ADDR_ACCY_LSB           33
-#define ADDR_ACCZ_MSB           34
-#define ADDR_ACCZ_LSB           35
-#define ADDR_GYRX_MSB           36
-#define ADDR_GYRX_LSB           37
-#define ADDR_GYRY_MSB           38
-#define ADDR_GYRY_LSB           39
-#define ADDR_GYRZ_MSB           40
-#define ADDR_GYRZ_LSB           41
-#define ADDR_MAGX_MSB           42
-#define ADDR_MAGX_LSB           43
-#define ADDR_MAGY_MSB           44
-#define ADDR_MAGY_LSB           45
-#define ADDR_MAGZ_MSB           46
-#define ADDR_MAGZ_LSB           47
-#define ADDR_HDG                48
-#define ADDR_TEMP9              49
-#define ADDR_ACCMODE            50
-#define ADDR_GYRMODE            51
-#define ADDR_CMPMODE            52
+#define ADDR_IMU_ACCX_MSB           30
+#define ADDR_IMU_ACCX_LSB           31
+#define ADDR_IMU_ACCY_MSB           32
+#define ADDR_IMU_ACCY_LSB           33
+#define ADDR_IMU_ACCZ_MSB           34
+#define ADDR_IMU_ACCZ_LSB           35
+#define ADDR_IMU_GYRX_MSB           36
+#define ADDR_IMU_GYRX_LSB           37
+#define ADDR_IMU_GYRY_MSB           38
+#define ADDR_IMU_GYRY_LSB           39
+#define ADDR_IMU_GYRZ_MSB           40
+#define ADDR_IMU_GYRZ_LSB           41
+#define ADDR_IMU_MAGX_MSB           42
+#define ADDR_IMU_MAGX_LSB           43
+#define ADDR_IMU_MAGY_MSB           44
+#define ADDR_IMU_MAGY_LSB           45
+#define ADDR_IMU_MAGZ_MSB           46
+#define ADDR_IMU_MAGZ_LSB           47
+#define ADDR_IMU_HDG                48
+#define ADDR_IMU_TEMP9              49
+#define ADDR_IMU_ACCMODE            50
+#define ADDR_IMU_GYRMODE            51
+#define ADDR_IMU_CMPMODE            52
 
 
 // Temp Probe Sensor
